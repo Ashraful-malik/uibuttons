b48d4308326a5bcebcf5c37dd4d5d57ff7a6acf1
Ashraful-malik <ashrafulmalik71@gmail.com>
2022-01-28 12:05:51 +0530

    env module added

--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,8 @@ import Navbar from "../components/Navbar.vue";
 export default {
   components: { Navbar },
   name: "IndexPage",
+  mounted() {
+    console.log(process.env.GOOGLE_ANALYTICS_ID);
+  },
 };
 </script>
