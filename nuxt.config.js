3278a09785f600b08c3f5c862acf085d1e72d204
Ashraful-malik <ashrafulmalik71@gmail.com>
2022-01-30 12:15:02 +0530

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -141,11 +141,13 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   // google analytic
+
   googleAnalytics: {
     id: `${process.env.GOOGLE_ANALYTICS_ID}`,
     disableScriptLoader: true,
     checkDuplicatedScript: true,
   },
+
   publicRuntimeConfig: {
     googleAnalytics: {
       id: `${process.env.GOOGLE_ANALYTICS_ID}`,
